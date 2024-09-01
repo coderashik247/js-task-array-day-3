9280baa5e992d768891966108fd69aea1ee20613
Ashik <ashikurashik20999@gmail.com>
2024-09-01 09:14:55 +0600

    task_2 added

---
+++ task_2.js
@@ -0,0 +1,9 @@
+const touristPlace =["Cox's Bazar", "Sundarbans", "Sajek Valley", "Saint Martin","Kuakata Sea Beach"];
+
+console.log(touristPlace);
+touristPlace.push("Rangamati Lake");
+console.log(touristPlace);
+touristPlace.push("Himchori National Park", "Guliakhali Beach");
+console.log(touristPlace);
+touristPlace.pop()
+console.log(touristPlace);
