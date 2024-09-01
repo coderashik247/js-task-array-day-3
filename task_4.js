550324b722dd5e4d31cd85589736f51c17ed5298
Ashik <ashikurashik20999@gmail.com>
2024-09-01 09:22:45 +0600

    task_4 added

---
+++ task_4.js
@@ -0,0 +1,18 @@
+const friendsName = ["Shawon", "Tanmoy", "Ariful", "Erfan", "Mitul"];
+
+const age = [23, 22, 24, 23, 25];
+
+const bool = [true, false , true, false, false];
+
+const member = "NaN"
+
+const Empty = [];
+
+const Blank = '[ ]';
+
+console.log(Array.isArray(friendsName));
+console.log(Array.isArray(age));
+console.log(Array.isArray(bool));
+console.log(Array.isArray(member));
+console.log(Array.isArray(Empty));
+console.log(Array.isArray(Blank));
