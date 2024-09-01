0b2802c29823dac7b1b3e462d4df7397f8cd969e
Ashik <ashikurashik20999@gmail.com>
2024-09-01 09:26:26 +0600

    task_3 added

---
+++ task_5.js
@@ -0,0 +1,10 @@
+const data1 = [1, 3, 5 , 7, 9];
+
+const data2 = [2, 4, 6, 8, 10];
+
+const data3 = [2, 3, 5, 7, 11];
+
+addArray1 = data1.concat(data2);
+console.log(addArray1);
+
+console.log(data3.concat(addArray1));
