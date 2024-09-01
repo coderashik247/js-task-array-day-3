7ff59a570e35bf0241b675e9d614fd4e87736da7
Ashik <ashikurashik20999@gmail.com>
2024-09-01 09:07:34 +0600

    task_1 added

---
+++ task_1.js
@@ -0,0 +1,6 @@
+const fruits = ["Apple", "Banana", "Dates", "Mango", "Orange", "Papaya"];
+
+console.log(fruits);
+console.log(fruits[3]); // 3rd index element 
+fruits[2] = "Jambura";
+console.log(fruits);
